97e2042a429fecf28b92658d3c3346e5128e6f41
Frank Miller <fmiller@esri.com>
2026-05-18 19:33:46 -0700

    Add CTestConfig.cmake

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+# Copyright (c) 2025 Environmental Systems Research Institute, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+set(CTEST_PROJECT_NAME link-what-you-include)
+set(CTEST_NIGHTLY_START_TIME 01:00:00 America/Los_Angeles)
+set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=link-what-you-include)
+set(CTEST_DROP_SITE_CDASH TRUE)
